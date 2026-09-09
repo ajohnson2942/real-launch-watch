1e54257fd64c1e90922a8179aee2dfae3a822ccb
ajohnson2942 <ajohnson2942@sdsu.edu>
2026-09-09 14:25:44 -0700

    Release v1.6.1 next tracked launch details tab

--- docs/v1.6.1-loader.js
+++
@@ -1,9 +0,0 @@
-(function(){
-  'use strict';
-  const version=document.getElementById('versionBadge');
-  if(version)version.textContent='v1.6.1';
-  const script=document.createElement('script');
-  script.src='v1.6.1-next-launch.js';
-  script.defer=true;
-  document.body.appendChild(script);
-})();
